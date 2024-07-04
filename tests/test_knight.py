62eda31593047c41bb079782407550371394aea0
RadDini <radu.dinicescu@softwire.com>
2024-07-04 15:50:40 +0300

    added knight out of bound tests

--- tests/test_knight.py
+++ tests/test_knight.py
@@ -120,7 +120,7 @@ class TestPawns:
         assert Square.at(5, 2) in moves
 
     @staticmethod
-    def test_knight_cant_move_top_board_on_edge():
+    def test_knight_cannot_move_top_board_on_edge():
         # Arrange
         board = Board.empty()
         knight = Knight(Player.BLACK)
@@ -137,7 +137,7 @@ class TestPawns:
         assert Square.at(8, 6) not in moves
 
     @staticmethod
-    def test_knight_cant_move_top_board_one_before_edge():
+    def test_knight_cannot_move_top_board_one_before_edge():
         # Arrange
         board = Board.empty()
         knight = Knight(Player.BLACK)
@@ -152,7 +152,7 @@ class TestPawns:
         assert Square.at(8, 5) not in moves
 
     @staticmethod
-    def test_knight_cant_move_bottom_board_on_edge():
+    def test_knight_cannot_move_bottom_board_on_edge():
         # Arrange
         board = Board.empty()
         knight = Knight(Player.BLACK)
@@ -169,7 +169,7 @@ class TestPawns:
         assert Square.at(-1, 6) not in moves
 
     @staticmethod
-    def test_knight_cant_move_bottom_board_one_before_edge():
+    def test_knight_cannot_move_bottom_board_one_before_edge():
         # Arrange
         board = Board.empty()
         knight = Knight(Player.BLACK)
@@ -184,7 +184,7 @@ class TestPawns:
         assert Square.at(-1, 5) not in moves
 
     @staticmethod
-    def test_knight_cant_move_right_board_on_edge():
+    def test_knight_cannot_move_right_board_on_edge():
         # Arrange
         board = Board.empty()
         knight = Knight(Player.BLACK)
@@ -201,7 +201,7 @@ class TestPawns:
         assert Square.at(6, 8) not in moves
 
     @staticmethod
-    def test_knight_cant_move_right_board_one_before_edge():
+    def test_knight_cannot_move_right_board_one_before_edge():
         # Arrange
         board = Board.empty()
         knight = Knight(Player.BLACK)
@@ -216,7 +216,7 @@ class TestPawns:
         assert Square.at(5, 8) not in moves
 
     @staticmethod
-    def test_knight_cant_move_left_board_on_edge():
+    def test_knight_cannot_move_left_board_on_edge():
         # Arrange
         board = Board.empty()
         knight = Knight(Player.BLACK)
@@ -233,7 +233,7 @@ class TestPawns:
         assert Square.at(6, -1) not in moves
 
     @staticmethod
-    def test_knight_cant_move_left_board_one_before_edge():
+    def test_knight_cannot_move_left_board_one_before_edge():
         # Arrange
         board = Board.empty()
         knight = Knight(Player.BLACK)
@@ -246,6 +246,39 @@ class TestPawns:
         # Assert
         assert Square.at(3, -1) not in moves
         assert Square.at(5, -1) not in moves
+
+    @staticmethod
+    def test_knight_cannot_take_same_color():
+        # Arrange
+        board = Board.empty()
+        knight = Knight(Player.BLACK)
+        knight_to_take = Knight(Player.BLACK)
+        square = Square.at(4, 4)
+        square_to_take = Square.at(5, 6)
+        board.set_piece(square, knight)
+
+        # Act
+        moves = knight.get_available_moves(board)
+
+        # Assert
+        assert Square.at(5, 6) not in moves
+
+    @staticmethod
+    def test_knight_can_take_other_color():
+        # Arrange
+        board = Board.empty()
+        knight = Knight(Player.BLACK)
+        knight_to_take = Knight(Player.WHITE)
+        square = Square.at(4, 4)
+        square_to_take = Square.at(5, 6)
+        board.set_piece(square, knight)
+
+        # Act
+        moves = knight.get_available_moves(board)
+
+        # Assert
+        assert Square.at(5, 6) in moves
+
 
 
 
