70f98c483601a77386aeac1c426c2e148952a4c7
RadDini <radu.dinicescu@softwire.com>
2024-07-04 14:58:24 +0300

    pawn can take diagonally

--- chessington/engine/pieces.py
+++ chessington/engine/pieces.py
@@ -38,16 +38,18 @@ class Pawn(Piece):
         square_list = []
         if not self.can_move(board, current_square):
             return square_list
+
         if self.player == Player.BLACK:
             square_list.append(Square.at(current_square.row - 1, current_square.col))
 
             if current_square.row == 6 and not self.has_piece_in_front(board, current_square, distance=2):
                 square_list.append(Square.at(current_square.row -2, current_square.col))
-
         else:
             square_list.append(Square.at(current_square.row + 1, current_square.col))
             if current_square.row == 1 and not self.has_piece_in_front(board, current_square, distance=2):
                 square_list.append(Square.at(current_square.row + 2, current_square.col))
+        square_list += self.get_moves_diag(board, current_square)
+
         return square_list
 
     def has_piece_in_front(self, board: Board, current_square, distance=1) -> bool:
@@ -70,8 +72,50 @@ class Pawn(Piece):
 
         return False
 
+    def has_piece_diagonal(self, board: Board, square: Square) -> bool:
+        if self.player == Player.BLACK:
+            diag_left = board.get_piece(Square.at(square.row-1, square.col-1))
+            diag_right = board.get_piece(Square.at(square.row-1, square.col+1))
+
+            if (diag_left and diag_left.player == Player.WHITE)\
+                    or (diag_right and diag_right.player == Player.BLACK):
+                return True
+        else:
+            diag_left = board.get_piece(Square.at(square.row + 1, square.col - 1))
+            diag_right = board.get_piece(Square.at(square.row + 1, square.col + 1))
+
+            if (diag_left and diag_left.player == Player.BLACK) \
+                    or (diag_right and diag_right.player == Player.BLACK):
+                return True
+        return False
+
+    def get_moves_diag(self, board: Board, square: Square) -> List[Square]:
+        square_list = []
+
+        if self.player == Player.BLACK:
+            diag_left = board.get_piece(Square.at(square.row - 1, square.col - 1))
+            diag_right = board.get_piece(Square.at(square.row - 1, square.col + 1))
+
+            if diag_left and diag_left.player == Player.WHITE:
+                square_list.append(Square.at(square.row - 1, square.col - 1))
+            if diag_right and diag_right.player == Player.WHITE:
+                square_list.append(Square.at(square.row - 1, square.col + 1))
+        else:
+            diag_left = board.get_piece(Square.at(square.row + 1, square.col - 1))
+            diag_right = board.get_piece(Square.at(square.row + 1, square.col + 1))
+
+            if diag_left and diag_left.player == Player.BLACK:
+                square_list.append(Square.at(square.row + 1, square.col - 1))
+            if diag_right and diag_right.player == Player.BLACK:
+                square_list.append(Square.at(square.row + 1, square.col + 1))
+
+        return square_list
+
     def can_move(self, board: Board, square: Square) -> bool:
-        return not (self.at_edge(square) or self.has_piece_in_front(board, square))
+        if not self.at_edge(square):
+            return self.has_piece_diagonal(board, square) or not self.has_piece_in_front(board, square)
+
+        return False
 
 
 
